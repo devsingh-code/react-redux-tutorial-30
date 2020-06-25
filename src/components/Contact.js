c3a9c83f10582e02231f831e14e8385b98193abc
Deovrat Singh <devsingh6n@gmail.com>
2020-06-25 21:51:20 +0530

    React router and Basic blog views

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Contact =(props) =>{
+    // console.log(props); //This is programmatic redirect
+    // setTimeout(() =>{
+    //     props.history.push('/about')
+    // },2000);
+
+    
+    return(
+
+        <div className="container">
+            <h4 className="center">Contact Page</h4>
+            <p>Materialize comes in two different forms. You can select which version you want depending on your preference and expertise. To start using Materialize, all you have to do is download one of the options below.</p>
+
+        </div>
+    )
+}
+
+
+export default Contact;
